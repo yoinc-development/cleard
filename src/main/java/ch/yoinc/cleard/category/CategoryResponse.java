149d15f3398eee3c9d25b3096ec6642f0003dfa9
Janes Thomas <work@yoinc.ch>
2026-09-21 00:56:19 +0200

    feat: create Category backend logic #7

--- src/main/java/ch/yoinc/cleard/category/CategoryResponse.java
+++ src/main/java/ch/yoinc/cleard/category/CategoryResponse.java
@@ -5,10 +5,20 @@ import java.math.BigDecimal;
 public record CategoryResponse(
         String id,
         String name,
-        String colour,
+        String color,
         String direction,
         BigDecimal monthToDateTotal,
         long monthToDateCount,
         BigDecimal warningThreshold
 ) {
+    public static CategoryResponse from(Category category, BigDecimal total, int rowSize) {
+        return new CategoryResponse(
+                category.getId().toString(),
+                category.getName(),
+                category.getColor(),
+                category.getDirection(),
+                total, rowSize,
+                category.getWarningThreshold() != null ? new BigDecimal(category.getWarningThreshold()) : null
+        );
+    }
 }
