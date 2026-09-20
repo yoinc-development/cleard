fd0c63460f82591aae297a918ff7b9452b323482
Janes Thomas <work@yoinc.ch>
2026-09-20 15:07:17 +0200

    chore: create home directory files when application is installed

--- src/main/java/ch/yoinc/cleard/DesktopApp.java
+++ src/main/java/ch/yoinc/cleard/DesktopApp.java
@@ -2,7 +2,10 @@ package ch.yoinc.cleard;
 
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +13,8 @@ import org.springframework.boot.web.server.context.WebServerInitializedEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
@@ -27,30 +32,57 @@ public class DesktopApp extends Application {
 
     private ConfigurableApplicationContext context;
     private int port;
+    private Throwable startupFailure;
 
     @Override
-    public void init() throws Exception {
+    public void init() {
         // The restart classloader fights with the JavaFX runtime; not wanted here anyway.
         System.setProperty("spring.devtools.restart.enabled", "false");
 
-        CompletableFuture<Integer> portFuture = new CompletableFuture<>();
-        SpringApplication app = new SpringApplication(CleardApplication.class);
-        app.addListeners((ApplicationListener<WebServerInitializedEvent>) event ->
-                portFuture.complete(event.getWebServer().getPort()));
+        try {
+            CompletableFuture<Integer> portFuture = new CompletableFuture<>();
+            SpringApplication app = new SpringApplication(CleardApplication.class);
+            app.addListeners((ApplicationListener<WebServerInitializedEvent>) event ->
+                    portFuture.complete(event.getWebServer().getPort()));
 
-        this.context = app.run();
-        this.port = portFuture.get(30, TimeUnit.SECONDS);
+            this.context = app.run();
+            this.port = portFuture.get(30, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            this.startupFailure = e;
+        }
     }
 
     @Override
     public void start(Stage stage) {
+        stage.setTitle("cleard");
+
+        if (startupFailure != null) {
+            stage.setScene(new Scene(startupFailurePane(startupFailure), 640, 400));
+            stage.show();
+            return;
+        }
+
         WebView webView = new WebView();
         webView.getEngine().load("http://localhost:" + port + "/");
-
-        stage.setTitle("cleard");
         stage.setScene(new Scene(webView, 1280, 800));
         // Native OS window with no browser chrome.
         stage.show();
+    }
+
+    private static VBox startupFailurePane(Throwable failure) {
+        StringWriter trace = new StringWriter();
+        failure.printStackTrace(new PrintWriter(trace));
+
+        Label heading = new Label("cleard failed to start");
+        heading.setStyle("-fx-font-size: 16px; -fx-font-weight: bold;");
+
+        Label detail = new Label(trace.toString());
+        detail.setWrapText(true);
+
+        VBox box = new VBox(12, heading, detail);
+        box.setAlignment(Pos.TOP_LEFT);
+        box.setStyle("-fx-padding: 16; -fx-background-color: white;");
+        return box;
     }
 
     @Override
